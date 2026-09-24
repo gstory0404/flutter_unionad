6264f5279dc1b487c4a7c4abe423178623483309
gstory <gstory0404@gmail.com>
2026-09-24 16:43:44 +0800

    * 重构插件

--- unionad_ios/ios/Classes/FlutterUnionadPlugin.m
+++ unionad_ios/ios/Classes/FlutterUnionadPlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterUnionadPlugin.h"
-#if __has_include(<flutter_unionad/flutter_unionad-Swift.h>)
-#import <flutter_unionad/flutter_unionad-Swift.h>
+#if __has_include(<unionad_ios/unionad_ios-Swift.h>)
+#import <unionad_ios/unionad_ios-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_unionad-Swift.h"
+#import "unionad_ios-Swift.h"
 #endif
 
 @implementation FlutterUnionadPlugin
@@ -13,4 +13,3 @@
   [SwiftFlutterUnionadPlugin registerWithRegistrar:registrar];
 }
 @end
-
